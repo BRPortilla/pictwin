b7da6428031405fb28562689c388bb0d01d3ead8
BRPortilla <benja.riverap.21@gmail.com>
2024-11-30 18:26:38 -0300

    test(repository test): add unit test of repository

---
+++ app/app/src/main/java/cl/ucn/disc/dsm/pictwin/data/repository/RepositoryTest.kt
@@ -0,0 +1,10 @@
+package cl.ucn.disc.dsm.pictwin.data.repository
+
+import cl.ucn.disc.dsm.pictwin.data.network.NetworkModule
+import kotlinx.coroutines.runBlocking
+//import org.junit.Before
+//import org.junit.Test
+import org.slf4j.LoggerFactory
+
+class RepositoryTest {
+}
